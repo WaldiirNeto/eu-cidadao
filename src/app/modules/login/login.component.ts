b6c18f52a7b7e9393b9c88dc15b780447919c05b
Feitoza de Azevedo <wrfv@gft.com>
2022-08-02 17:23:55 -0300

    #95 fadeIn nos formularios de login e redefinir senha

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { FormLoginModel } from './models/build-form';
 
@@ -5,17 +6,25 @@ import { FormLoginModel } from './models/build-form';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(2000, style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class LoginComponent extends FormLoginModel {
-  public loading: boolean
-  public hiddenForm: boolean = false
+  public loading: boolean;
+  public hiddenForm: boolean = false;
   constructor() {
-    super()
+    super();
   }
 
   public authentication(): void {}
 
   public closeForm(isShow: boolean): void {
-    this.hiddenForm = isShow
+    this.hiddenForm = isShow;
   }
 }
