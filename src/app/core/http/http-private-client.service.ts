ad0b89782aebbdb82ffc621846b339eccd1e9028
Feitoza de Azevedo <wrfv@gft.com>
2022-08-05 18:27:27 -0300

    adiciona lista de categorias no modal de criacao de assunto

--- src/app/core/http/http-private-client.service.ts
+++ src/app/core/http/http-private-client.service.ts
@@ -12,7 +12,7 @@ export class HttpPrivateClientService {
   }
 
   public get<T>(url: string): Observable<T> {
-
+    console.log(this.token)
     return this._http.get<T>(url, { headers: { 'Authorization': `Bearer ${this.token}` } })
   }
 }
--- src/app/private/modules/settings/pages/assuntos/components/modal-create-assunto/modal-create-assunto.component.ts
+++ src/app/private/modules/settings/pages/assuntos/components/modal-create-assunto/modal-create-assunto.component.ts
@@ -11,9 +11,9 @@ import { FormAssuntoModel } from '../../model/form-assunto.model'
 })
 export class ModalCreateAssuntoComponent extends FormAssuntoModel implements OnInit {
   protected loading: boolean
-  todo: Array<string> = []
+  listLeft: Array<string> = []
 
-  done: Array<string> = ['teste'];
+  listRight: Array<string> = [];
 
   constructor(@Inject(MAT_DIALOG_DATA) public assunto: Assunto) {
     super()
@@ -25,13 +25,18 @@ export class ModalCreateAssuntoComponent extends FormAssuntoModel implements OnI
 
   public populateListLeft(): void {
     const categoria = this.form.controls.categorias.value as string
-    this.todo.push(categoria)
+    this.listLeft.push(categoria)
   }
 
-  public removeItemListRight(item: string): void {
-    this.todo.push(item)
-    this.done = this.done.filter(categoria => categoria !== item)
+  public removeItemList(item: string, type: string): void {
+    if (type === 'right') {
+      this.listLeft.push(item)
+      this.listRight = this.listRight.filter(categoria => categoria !== item)
+    } else {
+      this.listLeft = this.listLeft.filter(categoria => categoria !== item)
+    }
   }
+
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex)
