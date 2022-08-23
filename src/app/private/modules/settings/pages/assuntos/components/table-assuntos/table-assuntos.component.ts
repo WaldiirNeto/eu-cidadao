32fb5e859f9872b5d8cf1baaa6f5b50242fe3fdc
Feitoza de Azevedo <wrfv@gft.com>
2022-08-23 11:43:40 -0300

    feat - ajuste na tabela de categorias

--- src/app/private/modules/settings/pages/assuntos/components/table-assuntos/table-assuntos.component.ts
+++ src/app/private/modules/settings/pages/assuntos/components/table-assuntos/table-assuntos.component.ts
@@ -1,6 +1,9 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core'
 import { MatCheckboxChange } from '@angular/material/checkbox'
 import { MatDialog } from '@angular/material/dialog'
+import { CategoriaModel, ListCategoriaModel, SubCategoriaOcorrenciaModel } from '@shared/models/categoria.model'
+import { CategoriasService } from '@shared/services/categorias.service'
+import { Subject, takeUntil } from 'rxjs'
 import { Assunto } from '../../model/assunto.model'
 import { ModalCreateAssuntoComponent } from '../modal-create-assunto/modal-create-assunto.component'
 import { ModalDeleteAssuntoComponent } from '../modal-delete-assunto/modal-delete-assunto.component'
@@ -10,81 +13,26 @@ import { ModalDeleteAssuntoComponent } from '../modal-delete-assunto/modal-delet
   templateUrl: './table-assuntos.component.html',
   styleUrls: ['./table-assuntos.component.scss']
 })
-export class TableAssuntosComponent implements OnInit {
+export class TableAssuntosComponent implements OnInit, OnDestroy {
 
   @Output() public tableEmit: EventEmitter<Array<number>> = new EventEmitter()
-  protected displayedColumns: string[] = ['assunto', 'responsavel', 'email', 'telefone', 'categorias', 'actions']
-  protected dataSource: Assunto[]
+  protected displayedColumns: string[] = ['assunto', 'categorias', 'actions']
+  protected listOcurrences: CategoriaModel[]
   private selectedCategories: Array<any> = []
-  constructor(private _dialog: MatDialog) { }
+  private _destroy$ = new Subject()
+  constructor(private readonly _dialog: MatDialog, private readonly _ocurrenceService: CategoriasService) { }
 
   ngOnInit(): void {
-    this.dataSource = [
-      {
-        id: 1,
-        assunto: `Água e Esgoto`,
-        categorias: ``,
-        email: `mussumIpsum@ipsum`,
-        responsavel_area: `Mususm ipsum`,
-        telefone: `00000000`
-      },
-      {
-        id: 2,
-        assunto: `Conservação e limpeza`,
-        categorias: ``,
-        email: `mussumIpsum@ipsum`,
-        responsavel_area: `Mususm ipsum`,
-        telefone: `00000000`
-      },
-      {
-        id: 3,
-        assunto: `Energia e iluminação`,
-        categorias: ``,
-        email: `mussumIpsum@ipsum`,
-        responsavel_area: `Mususm ipsum`,
-        telefone: `00000000`
-      },
-      {
-        id: 4,
-        assunto: `Escola e educação`,
-        categorias: ``,
-        email: `mussumIpsum@ipsum`,
-        responsavel_area: `Mususm ipsum`,
-        telefone: `00000000`
-      },
-      {
-        id: 5,
-        assunto: `Meio Ambiente`,
-        categorias: ``,
-        email: `mussumIpsum@ipsum`,
-        responsavel_area: `Mususm ipsum`,
-        telefone: `00000000`
-      },
-      {
-        id: 6,
-        assunto: `Saúde pública`,
-        categorias: ``,
-        email: `mussumIpsum@ipsum`,
-        responsavel_area: `Mususm ipsum`,
-        telefone: `00000000`
-      },
-      {
-        id: 7,
-        assunto: `Segurança pública`,
-        categorias: ``,
-        email: `mussumIpsum@ipsum`,
-        responsavel_area: `Mususm ipsum`,
-        telefone: `00000000`
-      },
-      {
-        id: 8,
-        assunto: `Trânsito e vias públicas`,
-        categorias: ``,
-        email: `mussumIpsum@ipsum`,
-        responsavel_area: `Mususm ipsum`,
-        telefone: `00000000`
-      }
-    ]
+    this.getListOcorrences()
+  }
+
+  private getListOcorrences(): void {
+    this._ocurrenceService.ListCategorias()
+      .pipe(takeUntil(this._destroy$))
+      .subscribe((categorias: ListCategoriaModel) => {
+        this.listOcurrences = categorias.lista
+        console.log(this.listOcurrences)
+      })
   }
 
   public openModalEdit(assunto: Assunto): void {
@@ -115,18 +63,22 @@ export class TableAssuntosComponent implements OnInit {
   }
 
   public populateFullArraySelected(event: MatCheckboxChange): void {
-    if (event.checked) {
-      this.selectedCategories = []
-      this.selectedCategories.push(...this.dataSource.map(category => category.id))
-    } else {
-      this.selectedCategories = []
-    }
-    this.tableEmit.emit(this.selectedCategories)
+    // if (event.checked) {
+    //   this.selectedCategories = []
+    //   this.selectedCategories.push(...this.dataSource.map(category => category.id))
+    // } else {
+    //   this.selectedCategories = []
+    // }
+    // this.tableEmit.emit(this.selectedCategories)
   }
 
   public checkDinamically(id: number): boolean {
     const check = this.selectedCategories.some(categoryId => categoryId === id)
     return check
+  }
+
+  public listSubCategories(subCategoriasOcorrencias: SubCategoriaOcorrenciaModel[]): string {
+    return subCategoriasOcorrencias.map(subCategoria => subCategoria.nome).join(',')
   }
 
   // private listCategoria(): void {
@@ -145,4 +97,9 @@ export class TableAssuntosComponent implements OnInit {
   //       }
   //     })
   // }
+  ngOnDestroy(): void {
+    this._destroy$.next(null)
+    this._destroy$.complete()
+  }
+
 }
